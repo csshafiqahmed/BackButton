82aa51b089f8931453d3f545bde7788105e1c131
Shafiq Ahmed <csshafiqahmed@gmail.com>
2020-11-23 23:37:30 +0500

    Initial commit

---
+++ app/src/main/java/com/codestown/backbutton/MainActivity.java
@@ -0,0 +1,25 @@
+package com.codestown.backbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button;
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/codestown/backbutton/MainActivity2.java
@@ -0,0 +1,16 @@
+package com.codestown.backbutton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        getSupportActionBar().setTitle("Second Activity");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
